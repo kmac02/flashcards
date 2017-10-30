f81d851e925f355ec376d25fdb4eeaa48373ac25
Kristin MacDonough & Melissa Kanhirun <pair+kmac02.mk7run@devbootcamp.com>
2017-10-30 10:54:33 -0500

    Create Deck model, routes and rootpath

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'decks#index'
+  resources :decks
   resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
